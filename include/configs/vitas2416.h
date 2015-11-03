1938a2bc7585eefff7d0f4557926dde7a91ec66d
xlongfeng <xlongfeng@126.com>
2015-11-03 11:26:45 +0800

    64bit vsprintf
    Signed-off-by: xlongfeng <xlongfeng@126.com>

--- include/configs/vitas2416.h
+++ include/configs/vitas2416.h
@@ -36,6 +36,8 @@
 
 #define CONFIG_DISPLAY_CPUINFO
 
+#define CONFIG_SYS_64BIT_VSPRINTF
+
 #define CONFIG_CMDLINE_TAG	 1	/* enable passing of ATAGs	*/
 #define CONFIG_SETUP_MEMORY_TAGS 1
 #define CONFIG_INITRD_TAG	 1
